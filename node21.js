cb7aca1a7bf567b827fc3c530c082e7cbce0c18f
kemermohammed <kemmah5833@gmail.com>
2023-09-07 23:24:10 +0300

    final

--- node21.js
+++ node21.js
@@ -3,5 +3,10 @@ axios.get('https://jsonplaceholder.typicode.com/posts')
 .then(function(response){
     console.log(response);
 })
+const axios = require("axios");
+axios.get('https://jsonplaceholder.typicode.com//posts?userId=1')
+.then(function(response){
+    console.log(response);
+})
 
   
